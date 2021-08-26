d85f17c7dd820fce25b2e269fb8a1144ce15a0dc
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-08-26 18:26:06 +0530

    commit

--- app/src/main/java/com/ecommerce/android/ourecom/MainActivity.java
+++ app/src/main/java/com/ecommerce/android/ourecom/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+//over
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
